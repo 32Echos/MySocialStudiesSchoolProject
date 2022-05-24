2047c89ac5f99149f7cec1db4fe5e39f9d7804f6
32Echos <105677009+32Echos@users.noreply.github.com>
2022-05-24 15:38:11 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1> The Mayan </h1>
+<h2> Ceremonies and Rituals </h2>
+<h5> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h5>
+<h3> Baptism </h3>
+<h4> A ritual</h4>
+<div> Baptism happened right after birth. The family would call a priest, that priest would read the babys hand and see his future. Seeing the babys future would help them know how to raise the baby.</div>
+<h5> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h5>
+<h3> Coming of age <h3>
+<h4> A ceremony </h4>
+<div> Girls went through this
+ceremony at age 12, boys at 14. This ceremony involved confessing. The priest cut a white bead from the boys hair and removed a string of red shells from around
+the girls waists. Boys and girls had worn these symbols of innocence since they were quite young. </div>
+<h5> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h5>
+<h3> Marriage Customs </h3>
+<h4> A ceremony </h4>
+<div> The bride and groom did not choose each other. Marriages were instead chosen by the village atanzahab or
+matchmaker. Families had to agree on how much food and clothing would be given to the bride’s family. They also
+had to agree on the number of years a young man would work for his new wifes family. </div>
+<h5> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h5>
+`;
